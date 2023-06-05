4fceed928cf0032bee26445d45f1e0c20c347a81
Sajjad Hoviegar <s.hoviegar@gmail.com>
2023-06-06 00:46:51 +0330

    adding computer algorithm to play with a human player

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Game from "./components/Game/Game";
 
 function App() {
-  return <Game numberOfBalls={25} />;
+  return <Game numberOfBalls={25} ballsMoveNumber={3} />;
 }
 
 export default App;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,39 +1,129 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Orb from "../Orb/Orb";
 import "./Game.css";
 
 const Game = (props) => {
   const numberOfOrbs = props.numberOfBalls;
+  const orbsMoveNumber = props.ballsMoveNumber;
 
-  const balls = [];
-  for (let i = 0; i < numberOfOrbs; i++) {
-    balls.push({ id: i, display: true });
-  }
-  balls.push({ id: 25, display: false });
-  balls.push({ id: 26, display: true });
+  const [orbs, setOrbs] = useState([]);
 
-  const [orbs, setOrbs] = useState(balls);
+  useEffect(() => {
+    const balls = [];
+    for (let i = 0; i < numberOfOrbs; i++) {
+      balls.push({ id: i, display: true });
+    }
+    setOrbs(balls);
+  }, [numberOfOrbs]);
 
-  //   useEffect(() => {
+  const randomNumberGenerator = (numberOfRandoms, balls) => {
+    let numArray = [];
 
-  //     setOrbs(balls);
-  //   }, []);
+    // populate numbers
+    for (let i = 0; i < balls.length; i++) {
+      // console.log(balls[i].display); // Check the value of display property
+      if (balls[i].display) {
+        numArray.push(i);
+      }
+    }
+    // console.log("numArray: ", numArray);
+
+    for (let j = balls.length - 1; j >= 0; j--) {
+      let swapIndex = Math.floor(Math.random() * j);
+      let tmp = numArray[swapIndex];
+      numArray[swapIndex] = numArray[j];
+      numArray[j] = tmp;
+    }
+
+    // console.log(numArray);
+    const results = numArray.filter((element) => {
+      return element !== undefined;
+    });
+    return results.slice(0, numberOfRandoms);
+  };
 
   const countActiveOrbs = () => {
     let count = 0;
-    for (let i = 0; i < numberOfOrbs; i++) {
-      if (orbs[i].display) count++;
-    }
+    orbs.map((orb) => {
+      if (orb.display) count++;
+    });
     return count;
+  };
+
+  const returnAllOrbs = () => {
+    const activeOrbs = [];
+    orbs.map((orb) => {
+      if (orb.display) activeOrbs.push(orb);
+      else activeOrbs.push(orb);
+    });
+    return activeOrbs;
   };
 
   const turnOffOrb = (event) => {
     // console.log(event.target.id);
     setOrbs((pervOrbs) => {
       const newOrbs = JSON.parse(JSON.stringify(pervOrbs));
-      newOrbs[event.target.id].display = !newOrbs[event.target.id].display;
+      // console.log(event.target.id);
+      for (let i = 0; i < newOrbs.length; i++) {
+        // console.log(newOrbs[i].id, event.target.id);
+        if (newOrbs[i].id == event.target.id) {
+          newOrbs[i].display = !newOrbs[i].display;
+        }
+      }
+
       return newOrbs;
     });
+  };
+
+  const computerPlay = () => {
+    const remainingBalls = countActiveOrbs();
+    const currentBalls = returnAllOrbs();
+
+    if (remainingBalls > 1) {
+      let rem = remainingBalls % (orbsMoveNumber + 1);
+      // console.log(remainingBalls, rem);
+      if (rem == 0) {
+        const randArr = randomNumberGenerator(orbsMoveNumber, currentBalls);
+        for (let i = 0; i < orbsMoveNumber; i++) {
+          currentBalls[randArr[i]].display = false;
+        }
+        // setTimeout(function () {
+        //   chooseBalls(orbsMoveNumber);
+        // }, 1500);
+        setOrbs(currentBalls);
+      }
+      if (rem == 1) {
+        const randMove = Math.floor(Math.random() * orbsMoveNumber) + 1;
+        const randArr = randomNumberGenerator(randMove, currentBalls);
+        for (let i = 0; i < randMove; i++) {
+          currentBalls[randArr[i]].display = false;
+        }
+        // setTimeout(function () {
+        //   chooseBalls(randMove);
+        // }, 1500);
+        setOrbs(currentBalls);
+      }
+      if (rem > 1) {
+        const randArr = randomNumberGenerator(rem - 1, currentBalls);
+        for (let i = 0; i < rem - 1; i++) {
+          currentBalls[randArr[i]].display = false;
+        }
+        // setTimeout(function () {
+        //   chooseBalls(rem - 1);
+        // }, 1500);
+        setOrbs(currentBalls);
+      }
+    } else if (remainingBalls == 1) {
+      // setTimeout(function () {
+      //   chooseBalls(1);
+      // }, 1500);
+      for (let i = 0; i < currentBalls.length; i++) {
+        if ((currentBalls[i].display = true)) currentBalls[i].display = false;
+      }
+
+      setOrbs(currentBalls);
+    }
+    //alert(currentBalls[0].mouseout);
   };
 
   return (
@@ -48,6 +138,7 @@ const Game = (props) => {
       ))}
       <br />
       Active Orbs: {countActiveOrbs()}
+      <button onClick={computerPlay}>End Turn</button>
     </div>
   );
 };
